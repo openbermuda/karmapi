b2f9ff4440995dbeb517969672d9636dbc6a5c81
John Gill <swfiua@gmail.com>
2016-04-04 14:40:49 -0300

    meta data generator for weather data

--- karmapi/meta/weather.py
+++ karmapi/meta/weather.py
@@ -1,22 +1,53 @@
+"""
+Utility to help generate meta data for a weather dataset.
+"""
+
+
+
 from datetime import date
 import karmapi.weather
 from karmapi.weather import RawWeather
-from karmapi.models.lat_lon_grid import LatLonGrid
 
 # defaults are good
 raw = RawWeather()
 
-META = dict(
-    karma = karmapi.weather,
-    model = LatLonGrid,
-    start = raw.start_day,
-    end   = raw.end_day,
+meta = dict(
+    start_year = raw.start_day.year,
+    start_month = raw.start_day.month,
+    start_day = raw.start_day.day,
+
+    end_year = raw.end_day.year,
+    end_month = raw.end_day.month,
+    end_day = raw.end_day.day,
+
     lats = raw.latitudes(),
     lons = raw.longitudes(),
 )
 
 # Stuff we can build
-BUILD = dict(
-    path="time/{year}/{month}/{day}/{field}",
-    karma=karmapi.weather,
+meta['builds'] = dict(
+    day = dict(
+        path="time/{year}/{month}/{day}/{field}",
+        karma="karmapi.weather.build_day",
+        model = "karmapi.models.lat_lon_grid.LatLonGrid",
+    )
 )
+
+
+# Stuff we can get
+meta['gets'] = dict(
+    day = dict(
+        path="time/{year}/{month}/{day}/{field}",
+        karma="karmapi.weather.get_day",
+        model = "karmapi.models.lat_lon_grid.LatLonGrid",
+     )
+)
+
+
+if __name__ == '__main__':
+
+    # write out meta data for dataset
+    import json
+    
+    with open('meta.json', 'w') as out:
+        out.write(json.dumps(meta, indent=True))
