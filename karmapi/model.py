141d29a0f059e054f15c54f2940465188aaa09c8
John Gill <swfiua@gmail.com>
2016-04-15 14:45:23 -0300

    docs for an event based model

---
+++ karmapi/model.py
@@ -0,0 +1,26 @@
+"""
+Data for an event based model.
+
+Event
+=====
+
+An event.  This is just an identifier and a location.
+
+Event Meta Data
+===============
+
+Meta data about the event, specific to the type of event.
+
+Hurricanes might have track and intensity at each part of the track. 
+
+Guess
+=====
+
+A set of events with timestamps.
+
+Model
+=====
+
+A set of guesses.
+
+"""
