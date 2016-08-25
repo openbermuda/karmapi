cf8dcdad5935795af49c11e2836a01f85bf70993
Johnny Gill <swfiua@gmail.com>
2016-08-25 14:29:23 -0300

    discover imageio, make movies

--- karmapi/show.py
+++ karmapi/show.py
@@ -1,3 +1,35 @@
 """
 Show images.
 """
+from IPython.display import HTML, Image
+import imageio
+
+from karmapi import base, heart
+
+
+def show(path):
+    """ Show image for path """
+    if path.endswith('.gif'):
+        return hshow(path)
+    
+    return Image(data=load(path))
+
+def hshow(path):
+    """ Show image for path using HTML 
+    
+    For some reason, IPython.display.Image does not do GIFs.
+
+    So we embed it in html instead.
+    """
+    return HTML('<img src="{}">'.format(path))
+
+
+def movie(images, **kwargs):
+    """ Create a movie for images """
+    return imageio.mimwrite(imageio.RETURN_BYTES, images, **kwargs)
+
+
+def load(path):
+
+    return imageio.imread(path)
+
