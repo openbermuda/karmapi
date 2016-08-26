5ffd4ef1f3b778cfd746d59115211ef5e76f6734
Johnny Gill <swfiua@gmail.com>
2016-08-26 13:56:35 -0300

    tankrain now a script that grabs bermuda weather radar

--- karmapi/tankrain.py
+++ karmapi/tankrain.py
@@ -1,6 +1,13 @@
 """ Bermuda weather
 """
 import datetime
+utcnow = datetime.datetime.utcnow
+
+from urllib.error import HTTPError
+
+from pathlib import Path
+
+from collections import defaultdict
 
 from karmapi import show
 
@@ -12,6 +19,8 @@ parish_template = 'Radar/RadarParish/{date:%Y-%m-%d-%H%M}_ParishRadar.png'
 
 atlantic_chart = 'surfaceAnalysis/Latest/Atlantic.gif'
 local_chart = 'surfaceAnalysis/Latest/Local.gif'
+
+target = 'tankrain/{date:%Y}/{date:%m}/{date:%d}/{name}_{date:%H%M}{suffix}'
 
 def get_images(now, template):
     """ Retrieve images currently available 
@@ -52,12 +61,34 @@ if __name__ == '__main__':
                 iurl = data['url'].format(date=timestamp,
                                          size=data['size'])
                 image = show.load(iurl)
-                print(iurl)
-                images[name].append(image)
-        except:
+                piurl = Path(iurl)
+                images[name].append(dict(image=image,
+                                         time=timestamp,
+                                         suffix=piurl.suffix))
+        except HTTPError as e:
             print('missing:', timestamp)
-            pass
+
+        except Exception as e:
+            raise e
+        
         timestamp -= (2 * aminute)
 
-    print(len(images))
     
+    for name, items in images.items():
+        for item in items:
+            date = item['time']
+            image = item['image']
+            suffix = item['suffix']
+
+            path = Path(target.format(
+                date=date,
+                suffix=suffix,
+                name=name))
+
+            print('Creating', path)
+            path.parent.mkdir(exist_ok=True, parents=True)
+
+            with path.open('wb') as outfile:
+                outfile.write(image)
+                    
+        
