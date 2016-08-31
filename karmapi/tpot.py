b19e02074596cdbaba9badabc7d61837490ca3af
John Gill <swfiua@gmail.com>
2016-08-30 21:04:50 -0300

    fill brew beer stew stir ferment

--- karmapi/tpot.py
+++ karmapi/tpot.py
@@ -8,11 +8,57 @@ path+ == hpat
 mastermind
 """
 
-def alpha():
+# tea
+A = None
+B = None
+P0 = None
+ALPHA = None
+BETA = None
+GAMMA = None
+
+# beer
+R_A = None
+R_B = None
+R_P0 = None
+R_ALPHA = None
+R_BETA = None
+R_GAMMA = None
+
+
+# ingredients
+OBSERVATIONS = None
+
+def _alpha():
     pass
 
-def beta():
+def _beta():
     pass
 
-def gamma():
+def _gamma():
+    pass
+
+
+def fill():
+    """ Fill the teapot """
+    global A, B, P0, ALPHA, BETA, GAMMA, OBSERVATIONS
+
+def brew():
+    """ Brew some T """
+    pass
+
+    
+def beer():
+    """ RE-Estimate B matrix
+
+    beer = reverse(reeb)
+    """
+    pass
+
+def stew():
+    pass
+
+def stir():
+    pass
+
+def ferment():
     pass
