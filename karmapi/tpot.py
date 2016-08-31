619b468b91135b872b02cef0011127509c6c6673
John Gill <swfiua@gmail.com>
2016-08-30 21:27:44 -0300

    more tea brewing

--- karmapi/tpot.py
+++ karmapi/tpot.py
@@ -28,36 +28,58 @@ R_GAMMA = None
 # ingredients
 OBSERVATIONS = None
 
-def _alpha():
+def alpha():
     pass
 
-def _beta():
+def beta():
     pass
 
-def _gamma():
+def gamma():
     pass
 
 
-def fill():
+def fill(parms):
     """ Fill the teapot """
     global A, B, P0, ALPHA, BETA, GAMMA, OBSERVATIONS
 
+    A = parms.a
+    B = parms.b
+    P0 = parms.p0
+    ALPHA = parms.alpha 
+    BETA = parms.beta
+    GAMMA = parms.gamma
+    ALPHA = parms.alpha
+
+
 def brew():
     """ Brew some T """
-    pass
-
+    global SCORE
+    
+    alpha()
+    beta()
+    SCORE = gamma()
     
 def beer():
     """ RE-Estimate B matrix
 
     beer = reverse(reeb)
-    """
-    pass
 
-def stew():
-    pass
+    Since we are in Bermuda, ginger beer perhaps.
+    """
+    global R_B, R_A, R_P0
+    R_B = bm_rest()
+    R_A = am_rest()
+    R_P0 = p0_rest()
 
 def stir():
+
+    global P0, A, B
+
+    A = R_A
+    B = R_B
+    P0 = R_P0
+
+def stew():
     pass
 
 def ferment():
