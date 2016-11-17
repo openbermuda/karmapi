eac81f57c22b0d5ab37c5d03d5d213f1218b1a24
Johnny Gill <swfiua@gmail.com>
2016-11-17 14:18:06 -0400

    add ignore option to match.matcher and fix buglet

--- karmapi/match.py
+++ karmapi/match.py
@@ -4,28 +4,30 @@
 
 from collections import defaultdict
 
-def matcher(a, b):
+def matcher(a, b, ignore=None):
     """ Find things in a that match things in b 
 
     Args:
 
        a: list of dictionaries to match
        b: list of dictionaries to find matches for
+       ignore: list of b keys to ignore in matching
     """
 
+    if ignore is None: ignore = set()
     result = defaultdict(list)
 
     for bx, item in enumerate(b):
         for ax, target in enumerate(a):
 
-            bkeys = set(item.keys())
+            bkeys = set(item.keys()) - ignore
             akeys = set(target.keys())
 
             # if b has keys not in a, then not a match
             if not bkeys.issubset(akeys): continue
 
             hit = True
-            for k in item:
+            for k in bkeys:
 
                 # if values don't match, not a match
                 if item[k] != target[k]:
@@ -45,6 +47,6 @@ def matcher(a, b):
         
         keepers = [x for x in hits if len(x) == minsize]
     
-        keep.append((a[ix], keepers))
+        keep.append((b[ix], keepers))
 
     return keep
