1d88a0e2853a8671815cedc21fdec37f057cefd6
Johnny Gill <swfiua@gmail.com>
2016-11-24 11:05:33 -0400

    yosser curio plan taking shape

--- karmapi/yosser.py
+++ karmapi/yosser.py
@@ -1,5 +1,4 @@
-"""
-Yosser, the builder.
+"""Yosser, the builder.
 
 Back in the early 80's the BBC made a mini series, "The boys from the
 blackstuff".  It was about builders working on the roads in Liverpool
@@ -23,10 +22,42 @@ So, yosser, meet curio.
 
 **Stands well back**
 
+So the idea is a micro service that listens for jobs.
+
+Let's call it *icandothat*
+
+Just send it your message and yosser will take care of the rest.
+
+So we need a server to send messages to, curio.socket should have most
+of what is needed.
+
+We need a queue to save messages in.   curio.queue works here.
+
+And we need a way to pop messages off the queue and turn them into tasks.
+The queue does the popping and so we just need to fire off tasks.
+
+Finally, we need some way to let clients know when the taks is done.
+
+Now the tasks are the tricky bit, because they might take a while.
+
+They just need to farm out the actual work to another process.  This
+looks to be the job of curio.workers.  run_in_process() sounds like it
+might do the trick.
+
+No threads, otherwise there will be stinging bats.
+
+This thing should just run.
+
 """
 import curio
 
+from curio import socket, queue, workers
+
 from karmapi import base
+
+# last in first out seems right for yosser
+# but there is also queue.EpicQueue -- not sure if that comes with bats.
+lifo = queue.LifoQueue()
 
 def look_for_a_job():
     """ Returns the path of something to build """
@@ -56,4 +87,13 @@ def sleep():
     raise NotImplemented
 
 
+def run():
+    """ Start yosser runnint """
 
+    # create a server
+    server = icandothat()
+
+    # queue
+    queue = queue.EpicQueue()
+
+    # now make it work...
