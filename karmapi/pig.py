41c79dfbb5354a60814fbef8bf5b11d235de2d5a
Johnny Gill <swfiua@gmail.com>
2016-11-28 16:30:35 -0400

    pig gets lipstick

--- karmapi/pig.py
+++ karmapi/pig.py
@@ -13,9 +13,12 @@ from PyQt5 import QtWidgets as qtw
 
 from PyQt5.QtCore import Qt as qt
 
-from PyQt5.QtWidgets  import (
-    QApplication, QWidget, QLabel, QLineEdit, QTextEdit,
-    QGridLayout, QBoxLayout, QHBoxLayout, QVBoxLayout)
+from PyQt5 import QtCore as qtcore
+
+from matplotlib.backends.backend_qt5agg import (
+    FigureCanvasQTAgg as FigureCanvas)
+
+from matplotlib.figure import Figure
 
 from karmapi import base
 
@@ -145,7 +148,7 @@ class Yosser(qtw.QWidget):
         
         rows = [[Plotter, Data], [Docs, Console]]
         rows = [[Console, Console], [Console, Console]]
-        rows = [[MyDynamicMplCanvas, MyStaticMplCanvas]]
+        rows = [[Image, Image]]
 
         # FIXME create the widget
         vlayout = qtw.QVBoxLayout(parent)
@@ -158,23 +161,137 @@ class Yosser(qtw.QWidget):
                 hlayout.addWidget(item(None))
 
 class Image(MyStaticMplCanvas):
-    """ An image widget 
-    """
-    pass
+    """ An image widget
 
-class Video(MyDynamicMplCanvas):
+    This is just a wrapper around matplotlib FigureCanvas.
+    """
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        # We want the axes cleared every time plot() is called
+        self.axes.hold(False)
+
+        self.compute_initial_figure()
+
+        #
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   qtw.QSizePolicy.Expanding,
+                                   qtw.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+    def compute_initial_figure(self):
+        """ Over-ride to draw what you want 
+        
+        For example:
+        
+          t = arange(0.0, 3.0, 0.01)
+          s = sin(2*pi*t)
+          self.axes.plot(t, s)
+        """
+        import pandas
+        self.axes.imshow(pandas.np.random.random(size=(100, 100)))
+        
+
+    def show(self, image):
+        """ Display an image """
+        self.axes.imshow(image)
+
+
+
+class Video(Image):
     """ a video widget 
-    """
-    pass
 
-class Table:
+    This is currently a matplotlib FigureCanvas
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+        timer = qtcore.QTimer(self)
+        timer.timeout.connect(self.update_figure)
+        timer.start(1000)
+
+    def compute_initial_figure(self):
+        self.axes.plot([0, 1, 2, 3], [1, 2, 0, 4], 'r')
+
+    def update_figure(self):
+        # Build a list of 4 random integers
+        # between 0 and 10 (both inclusive)
+        l = [random.randint(0, 10) for i in range(4)]
+
+        self.axes.plot([0, 1, 2, 3], l, 'r')
+        self.draw()
+
+
+class Table(qtw.QTableView):
     """ A table, time for dinner 
 
-    matplotlib has a table widget.  It will do for now.
+    Using QTableView
 
-    .. have to go find it first.
+    The data side of this is in PandasModel below.
+
+
+    To attach the data just do:
+
+    tab = Table()
+    tab.setModel(PandasModel(df))
+
+    If filtering and more is needed there is always this:
+
+    https://gist.github.com/jsexauer/f2bb0cc876828b54f2ed    
     """
-    pass
+    def __init__(self):
+
+        super().__init__()
+        self.setSortingEnabled(True)
+
+
+class PandasModel(qtcore.QAbstractTableModel):
+    def __init__(self, data, parent=None):
+        qtcore.QAbstractTableModel.__init__(self, parent)
+        self._data = data
+
+    def rowCount(self, parent=None):
+        return len(self._data.values)
+
+    def columnCount(self, parent=None):
+        return self._data.columns.size
+
+    def data(self, index, role=qt.DisplayRole):
+        if index.isValid():
+            if role == qt.DisplayRole:
+                # FIXME -- format this pretty
+                data = self._data.values[index.row()][index.column()]
+                try:
+                    value = '{:,}'.format(data)
+                except:
+                    value = str(data)
+                return qtcore.QVariant(value)
+        return qtcore.QVariant()
+
+
+    def headerData(self, section, orientation, role):
+        """ Return header for given column """
+        if role == qt.DisplayRole and orientation == qt.Horizontal:
+
+            return self._data.columns.values[section]
+
+        return qtcore.QAbstractTableModel.headerData(
+            self, section, orientation, role)
+
+    def sort(self, ncol, order):
+        """Sort table by given column number. """
+        self.layoutAboutToBeChanged.emit()
+
+        self._data.sort_values(by=self._data.columns[ncol], 
+                               ascending=order == qt.AscendingOrder,
+                               inplace=True)
+        
+        self.layoutChanged.emit()
+
+
 
 
 def build(recipe, parent=None, row=True):
@@ -204,7 +321,7 @@ def build_widget(item):
     return QLabel(item.get(name, 'noname'))
 
 
-class PiWidget(QWidget):
+class PiWidget(qtw.QWidget):
 
     def __init__(self, recipe):
 
@@ -225,7 +342,7 @@ def run(recipe):
 
 
     print(sys.argv)
-    app = QApplication(sys.argv)
+    app = qtw.QApplication(sys.argv)
 
     
     title = recipe.get('title', app.applicationName())
