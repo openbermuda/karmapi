9af036c418204b73e705a2d379aebeeec3470c24
Johnny Gill <swfiua@gmail.com>
2017-01-31 19:44:22 -0400

    start pigfarm curio

--- karmapi/curit.py
+++ karmapi/curit.py
@@ -1,11 +1,61 @@
-# hello.py
+"""
+Goal here is to have a thread launching piglets.
+
+And curio controlling the operation.
+
+Aim to use joy to control which widget loop to use.
+
+No piglets known to be harmed with this code.
+"""
+from karmapi import pig
 import curio
+import tkinter
 
-async def countdown(n):
-    while n > 0:
-        print('T-minus', n)
-        await curio.sleep(1)
-        n -= 1
 
-if __name__ == '__main__':
-    curio.run(countdown(10))
+
+class PigFarm:
+    """ A pig farm event loop """
+
+    def __init__(self, meta=None, events=None):
+
+        self.event = events # curio.UniversalQueue()
+
+        self.piglet_event = curio.UniversalQueue()
+
+        self.piglets = curio.UniversalQueue()
+
+        # start a gui eventloop
+        pass
+
+
+
+    def build(self, meta):
+        """ Do the piglet build """
+
+        piglet = backend.build(meta)
+
+        self.piglets.push(piglet)
+
+
+
+    async def run(self):
+        """ Make the pigs run """
+
+        # spawn a task for each piglet
+
+        # spawn a task to deal with keyboard events
+
+        # spawn a task to deal with mouse events
+
+        # ... spawn tasks to deal with any events
+        
+
+        while True:
+            while self.piglets:
+                # spawn a task for each piglet
+                piglet = self.piglets.pop()
+
+                await curio.spawn(piglet.run())
+
+            # wait for an event
+            event = await self.event.pop()
