6c5bd62c980acb71b8d7e50af2f87040d1bc8490
Kallan Si <infinityslalom@gmail.com>
2017-02-02 17:21:03 -0400

    add beginnings of sound processing

---
+++ karmapi/hush.py
@@ -0,0 +1,37 @@
+from matplotlib import pyplot
+import pyaudio
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+def get_stream():
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    return stream
+
+def record(stream):
+
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    return frames
+
+
+
+
+
+
