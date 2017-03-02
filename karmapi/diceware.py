9bf5f519a88fd949c6a3bab5aa53ce1300f280fe
Johnny Gill <swfiua@gmail.com>
2017-03-01 22:33:26 -0400

    random walk to dice.. too many arguments somewhere

--- karmapi/diceware.py
+++ karmapi/diceware.py
@@ -111,31 +111,38 @@ class StingingBats(pig.Canvas):
         self.canvas.configure(scrollregion=(0, 0, width, height))
 
 
-    def draw_digit(self, x, y, n, die):
+    def draw_digit(self, x, y, size, die):
         """ Draw a digit like a die """
         digits = {
-            1: (0,0,0, 0,1,0, 0,0,0),
-            2: (1,0,0, 0,0,0, 0,0,1),
-            3: (1,0,0, 0,1,0, 0,0,1),
-            4: (1,0,1, 0,0,0, 1,0,1),
-            5: (1,0,1, 0,1,0, 1,0,1),
-            6: (1,0,1, 1,0,1, 1,0,1)}
+            1: ((0,0,0), (0,1,0), (0,0,0)),
+            2: ((1,0,0), (0,0,0), (0,0,1)),
+            3: ((1,0,0), (0,1,0), (0,0,1)),
+            4: ((1,0,1), (0,0,0), (1,0,1)),
+            5: ((1,0,1), (0,1,0), (1,0,1)),
+            6: ((1,0,1), (1,0,1), (1,0,1))}
 
-        self.draw_pixel(digits[die], x, y, 10, 10)
+        
+        self.draw_pixel(digits[die], x, y, size, size, 10, 10)
 
     def draw_pixel(self, data, x, y, width, height):
 
         for ix, row in enumerate(data):
             for jx, col in enumerate(row):
 
-                colour = colours[random.randint(0, len(colours) - 1)]
+                colour = self.random_colour()
                 
-                xx = (ix * gap) + x
+                xx = (ix * width) + x
                 
-                yy = (jx * gap) + x
+                yy = (jx * height) + x
                 self.canvas,create_arc(xx-size, yy-size, xx+size, yy+size,
                      start=0, extent=360, fill=colour)
-        
+
+    def random_colour(self):
+
+        colours = self.theme.colours
+
+        return colours[random.randint(0, len(colours) - 1)]
+
         
     def draw_dice(self):
         """ Draw the dice """
@@ -143,10 +150,13 @@ class StingingBats(pig.Canvas):
         #FIXME make the tkinter part async
 
         gap = self.width / (1 + self.size)
+
+        xx = gap / 2
+        yy = self.height * 0.3
+        
         for die in self.data:
-
-            self.draw_digit(x, y, self.size, die)
-
+            self.draw_digit(xx, yy, gap * 0.4, die)
+            self.xx += gap
             
 
     async def run(self):
