1c8cd13972c74d40e7b1b13591efc19e38ac9936
Johnny Gill <swfiua@gmail.com>
2017-03-02 11:13:55 -0400

    docs for grandpa eric

---
+++ karmapi/eric.py
@@ -0,0 +1,32 @@
+""" Eric - grandpa time machine
+
+
+Idle (named after python Eric Idle)
+
+Thing of Eric as grandpa.  Somewhere you can go to find new, maybe
+old, ways of doing things.
+
+grandpa is the one who shows a grandchild how to make jelly after a
+hurricane when the electricity is gone and the fridge is off.
+
+Make a cradle with string and lower the bowl of jelly so it rests in
+the water of the water tank.  This is cool, after a couple of hours
+the jelly will be set.
+
+No jelly here, but Idle: an interactive python promt.
+
+Come here to import a module, see what it does, read the code.
+
+Change the code, see what happens.
+"""
+
+import idlelib
+
+from karmapi import piglet
+
+class Eric(piglet.Pig):
+    """ An async python console, using IDLE """
+    pass
+    
+
+
