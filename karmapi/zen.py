5e4cc388373d581a9236d3cff6dcda9f5c4dda22
Johnny Gill <swfiua@gmail.com>
2017-03-10 20:25:39 -0400

    zen of python code

---
+++ karmapi/zen.py
@@ -0,0 +1,16 @@
+"""
+Many years agp Tim Peters wrote "The Zen of Python".
+
+It was turned into a module. 
+
+If you "import this" you get the Zen of Python.
+
+If you are running *eric* or *IDLE* then just "Control-m", it should prompt you for a module name.
+
+Enter *this* and it should show you the code for the *this* module.
+
+
+"""
+
+
+import this
