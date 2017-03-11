5b3e6fb7bdfc42493c0e4cf6756e5fe1268cf2a7
Johnny Gill <swfiua@gmail.com>
2017-03-10 20:29:11 -0400

    zen pints and karma

--- karmapi/zen.py
+++ karmapi/zen.py
@@ -9,6 +9,19 @@ If you are running *eric* or *IDLE* then just "Control-m", it should prompt you 
 
 Enter *this* and it should show you the code for the *this* module.
 
+pint
+====
+
+A few days back we talked about assigning pi points, or pints for short to things we could do here. 
+
+For now they don't really exist in any real fashion, just imaginary.
+
+zen
+===
+
+A blockchain that gives zen for karma.
+
+Initial mining by a network of pi's.
 
 """
 
