88f524f33d763addb080b7ea400b4ba979a7a90f
Johnny Gill <swfiua@gmail.com>
2017-04-07 16:41:46 -0300

    generators async await sense emulator too

--- karmapi/sense.py
+++ karmapi/sense.py
@@ -22,12 +22,20 @@ FIXME:
 
 """
 import subprocess
-import curio
-import sense_hat
-import pandas
 import datetime
+import csv
+import time
+
+import curio
+import pandas
 
 from karmapi import pigfarm
+
+try:
+    import sense_hat
+except:
+    # see if sense hat emulator is around
+    import sense_emu as sense_hat
 
 
 def stats(hat):
@@ -41,49 +49,64 @@ def stats(hat):
 
 def get_stats(hat):
     """ Return weather, compass and gyro data"""
-    data = get_weather(hat)
+    data = next(get_weather(hat))
 
-    data.update(get_compass(hat))
+    compass = next(get_compass(hat))
+    data.update(compass)
 
-    data.update(get_gyro(hat))
+    gyro = next(get_gyro(hat))
+    data.update(gyro)
 
     return data
 
 def get_weather(hat):
-    
-    data = dict(
-        temp = hat.temp,
-        humidity = hat.humidity,
-        pressure = hat.pressure,
-        temperature_from_pressure = hat.get_temperature_from_pressure(),
-        temperature_from_humidity = hat.get_temperature_from_humidity(),
-        cpu_temperature = get_cpu_temperature(),
-        )
-    guess = data['temperature_from_pressure'] + data['temperature_from_humidity']
-    guess = guess / 2.0
 
-    cputemp = data['cpu_temperature']
+    while True:
+        data = dict(
+            temp = hat.temp,
+            humidity = hat.humidity,
+            pressure = hat.pressure,
+            temperature_from_pressure = hat.get_temperature_from_pressure(),
+            temperature_from_humidity = hat.get_temperature_from_humidity(),
+            cpu_temperature = get_cpu_temperature(),
+            )
+        guess = data['temperature_from_pressure'] + data['temperature_from_humidity']
+        guess = guess / 2.0
 
-    guess = guess - ((cputemp - guess) / 2)
+        cputemp = data['cpu_temperature']
 
-    data['temperature_guess'] = guess
+        guess = guess - ((cputemp - guess) / 2)
 
-    data['timestamp'] = datetime.datetime.now()
+        data['temperature_guess'] = guess
 
-    return data
+        data['timestamp'] = time.time()
+
+        yield data
 
 
 def get_gyro(hat):
 
-    return hat,gyro
+    while True:
+        data = hat,gyro
+
+        data['timestamp'] = time.time()
+
+        return data
+
 
 def get_acceleration(hat):
 
-    return hat,accel
+    while True:
+        data = hat,accelwriter
+    
+        data['timestamp'] = time.time()
+
+        yield data
 
 def get_compass(hat):
 
-    return dict(compass=hat.compass)
+    while True:
+       yield dict(compass=hat.compass, timestamp=time.time())
     
 
 def get_cpu_temperature():
@@ -105,6 +128,8 @@ def show_all_stats(hat, show=None):
 
         for key, value in stats.items():
             show("%s: %04.1f\n" % (key, value))
+
+            
 
 class WeatherHat(pigfarm.MagicCarpet):
     """  Sense Hat widget """
@@ -178,9 +203,94 @@ class Monitor(pigfarm.MagicCarpet):
     def plot(self):
         pass
 
-            
+def get_outfile(path, name):
+    """ Open output file """
+    path = Path(path)
+
+    now = datetime.datetime.now()
+
+    path = path / f'{now.year}' / f'{now.month}' / f'{now.day}' / name
+
+    path.parent.mkdir(exist_ok=True, parents=True)
+
+    if path.exists():
+        # hmm... open in append, but then don't need a header
+        header = False
+
+        outfile = path.open('a')
+
+        return header, outfile
+
+
+    header = True
+    return header, path.open('w')
+
+        
+
+def get_writer(path, name, data, sleep):
+
+    header = next(data).keys()
+
+    # open outfile
+    need_header, outfile = get_outfile(path, name)
     
-if __name__ == '__main__':
+    writer = csv.DictWriter(outfile, fieldnames=header)
+
+    if need_header:
+        writer.writeheader()
+    
+    curio.run(recorder(reader, writer, sleep))
+
+async def recorder(path, name, data, sleep=1):
+    """ Record from a sensor 
+
+    path: base folder to put stuff
+    name: the name for the values
+    data: an iterator over dictionaries
+    """
+
+    writer = get_writer(path, name, data, sleep)
+
+    while True:
+        writer.writerow(reader())
+        await curio.sleep(sleep)
+
+
+
+async def record(path='.', sleep=1):
+    """ Record everything from the hat """
+    hat = sense_hat.SenseHat()
+
+    weather = get_weather(hat)
+    compass = get_compass(hat)
+    gyro = get_gyro(hat)
+    accel = get_acceleration(hat)
+
+    tasks = [weather, compass, gyro, accel]
+    names = ['weather', 'compass', 'gyro', 'accel']
+
+    # magic from curio
+    async with curio.TaskGroup(wait='any') as workers:
+        
+        for task, name in zip(tasks, names):
+
+            await workers.spawn(path, name, task, sleep)
+
+def main():
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--pig', action='store_true')
+    parser.add_argument('--path', default='.')
+    parser.add_argument('--sleep', type=float, default=1)
+
+    args = parser.parse_args()
+    
+    if not args.pig:
+        curio.run(record(args.path, args.sleep))
+        return
 
     farm = pigfarm.PigFarm()
 
@@ -189,7 +299,12 @@ if __name__ == '__main__':
     farm.add(WeatherHat)
     farm.add(OrientHat)
 
+    from karmapi.mclock2 import GuidoClock
+    farm.add(GuidoClock)
+
     pigfarm.run(farm)
     
-        
     
+if __name__ == '__main__':
+
+    main()
