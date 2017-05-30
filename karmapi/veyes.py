c6cd376c80aedd4d68709061afedb0d7b8908c6c
Johnny Gill <swfiua@gmail.com>
2017-05-30 14:35:23 +0000

    v eyes image capture

--- karmapi/veyes.py
+++ karmapi/veyes.py
@@ -1,6 +1,9 @@
 """
 Eyes for pies.
 """
+import argparse
+import datetime
+from pathlib import Path
 
 from picamera import PiCamera
 import time
@@ -9,31 +12,61 @@ import curio
 from fractions import Fraction
 
 
-async def long_exposure(path,
-                  length=6,
+def long_exposure(length=6,
                   framerate=6,
                   resolution=(1280, 720),
-                  iso=800,
-                  sleep=60):
+                  iso=800):
     """ Take a long exposure shot
 
-
-    path: where to save the image
     length: exposure length in seconds
     framerate: number of frames per second
     resolution: resolution for image
+    iso: iso setting for camera
     """
     # Set a framerate of 1/6fps, then set shutter
     # speed to 6s and ISO to 800
     camera = PiCamera(resolution=resolution, framerate=Fraction(1, framerate))
     camera.shutter_speed = length * 1000000
     camera.iso = 800
-    # Give the camera a good long time to set gains and
-    # measure AWB (you may wish to use fixed AWB instead)
-    await curio.sleep(sleep)
 
     camera.exposure_mode = 'off'
     # Finally, capture an image with a 6s exposure. Due
     # to mode switching on the still port, this will take
     # longer than 6 seconds
     camera.capture(path)
+
+
+async def capture(args):
+
+    if args.long:
+        camera = long_exposure()
+    else:
+        camera = PiCamera()
+
+    camera.start_preview()
+    while True:
+
+        now = datetime.datetime.now()
+        path = Path(f'{args.path}/{now.year}/{now.month}/{now.day}')
+        path.mkdir(exist_ok=True, parents=True)
+        path = path / f'{now.hour:02}{now.minute:02}{now.second:02}.jpg'
+
+        print(path)
+        camera.capture(str(path))        
+        await curio.sleep(args.sleep)
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--sleep', type=float, default=60)
+    parser.add_argument('path', nargs='?', default='.')
+    parser.add_argument('--long', action='store_true')
+
+    args = parser.parse_args()
+
+    curio.run(capture(args))
+
+
+if __name__ == '__main__':
+
+    main()
