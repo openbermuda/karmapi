046078b8b2bbf84bfca2d6e143bc9c6f7223d85b
Johnny Gill <swfiua@gmail.com>
2017-08-24 18:10:05 -0300

    ncdf bumbling along

--- karmapi/ncdf.py
+++ karmapi/ncdf.py
@@ -6,7 +6,6 @@ import math
 
 import netCDF4
 
-
 def load(path):
 
     return netCDF4.Dataset(path)
@@ -15,3 +14,18 @@ def load(path):
 def images(path, folder):
 
     df = load(path)
+
+def generate_data(stamps, values):
+
+    stamps = df.variables['time']
+
+    epoch = datetime.date(1900, 1, 1)
+    
+    for ix, stamp in enumerate(stamps):
+
+        date = epoch + datetime.timedelta(hours=stamp)
+
+        yield values[ix], date
+    
+
+        
