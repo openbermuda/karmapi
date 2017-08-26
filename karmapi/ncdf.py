2525b36723752bdad8b9c4a4654a794192023a65
Johnny Gill <swfiua@gmail.com>
2017-08-26 10:51:27 -0300

    working on turning netcdf into images

--- karmapi/ncdf.py
+++ karmapi/ncdf.py
@@ -3,8 +3,12 @@ Interface to netcdf files
 """
 import datetime
 import math
+import argparse
+from pathlib import Path
 
 import netCDF4
+
+from matplotlib import pyplot
 
 def load(path):
 
@@ -27,3 +31,46 @@ def generate_data(stamps, values):
     
 
         
+def images(path, stamps, values):
+
+    for data, date in generate_data(stamps, values):
+
+        pyplot.imshow(data)
+
+        path = Path(f'{path}/{date.year}/1/1/').expanduser()
+
+        path.mkdir(exist_ok=True, parents=True)
+
+        path = path / f'{date.hour:02}{date.minute:02}{date.second:02}.jpg'
+        
+        pyplot.savefig(path, bbox_inches='tight', pad_inches=0)
+
+
+
+if __name__ == '__main__':
+
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--path', default='karmapi/ecmwf')
+    parser.add_argument('--value', default='t2m')
+    parser.add_argument('--raw', default='temperature.nc')
+
+    args = parser.parse_args()
+
+    path = Path(args.path)
+
+    df = load(path / args.raw)
+
+    stamps = df.variables['time']
+
+    values = df.variables[args.value]
+
+    path = path / args.value
+
+    images(path, stamps, values[:10])
+    
+
+    
+
+    
