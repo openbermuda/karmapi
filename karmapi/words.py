9d6715c59e3add2bc6057d15d78a7f4a7377f97a
Johnny Gill <swfiua@gmail.com>
2017-10-16 00:53:18 -0300

    topn words

---
+++ karmapi/words.py
@@ -0,0 +1,42 @@
+"""  count words
+"""
+
+from collections import Counter
+
+from pathlib import Path
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('path', nargs='+')
+
+parser.add_argument('--glob', default='**/*.rst')
+
+args = parser.parse_args()
+
+
+totals = Counter()
+
+for path in args.path:
+    print(path)
+    for name in Path(path).glob(args.glob):
+
+        print(name)
+
+        counts = Counter()
+
+        counts.update(name.open().read().split())
+
+        print(counts.most_common(5))
+        print(sum(counts.values()))
+        print()
+
+        totals.update(counts)
+
+print('Totals:')
+
+print(totals.most_common(20))
+
+print(sum(totals.values()))
+        
