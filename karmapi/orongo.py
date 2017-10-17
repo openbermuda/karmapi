f24441a9c3aa6d72a655633ccf50ef5dccd4e624
Johnny Gill <swfiua@gmail.com>
2017-10-16 22:17:47 -0300

    round and round days weeks and months in rongo rongo land

--- karmapi/orongo.py
+++ karmapi/orongo.py
@@ -15,9 +15,21 @@ from pathlib import Path
 
 import argparse
 
-import math
+from math import pi, e
 
 import random
+
+YEAR = 1
+
+MONTH = 4
+
+WEEK = 52
+
+DAY = 7
+
+EMONTH = (DAY * MONTH) / (pi * e)
+
+EYEAR = 0.25 / (DAY * WEEK)
 
 def orongo(data):
     """ Spin it around """
@@ -25,45 +37,56 @@ def orongo(data):
 
     return reversed(data)
 
+def alpha(image):
+    """ Pick symbols from an image """
+
+    for season in range(MONTH):
+        for week in range(MONTH):
+            for day in range(DAY):
+                # pick up a pick up a pixel or many from image
+                yield None
+            
 
 
-parser = argparse.ArgumentParser()
+if __name__ == '__main__':
 
-parser.add_argument('path', nargs='+')
+    parser = argparse.ArgumentParser()
 
-parser.add_argument('--glob', default='**/*.rst')
+    parser.add_argument('path', nargs='+')
 
-args = parser.parse_args()
+    parser.add_argument('--glob', default='**/*.rst')
 
-
-totals = Counter()
-ototals = Counter()
-
-for path in args.path:
-    print(path)
-    for name in Path(path).glob(args.glob):
-
-        print(name)
-
-        counts = Counter()
-
-        data = name.open().read()
-        counts.update(data.split())
-        totals.update(counts)
+    args = parser.parse_args()
 
 
-        rongo = orongo(data)
-        counts = Counter(rongo)
+    totals = Counter()
+    ototals = Counter()
 
-        print('rongo')
-        print(counts.most_common(5))
-        print(sum(counts.values()))
-        print()
+    for path in args.path:
+        print(path)
+        for name in Path(path).glob(args.glob):
+
+            print(name)
+
+            counts = Counter()
+
+            data = name.open().read()
+            counts.update(data.split())
+            totals.update(counts)
 
 
-print('Totals:')
+            rongo = orongo(data)
+            counts = Counter(rongo)
 
-print(totals.most_common(20))
+            print('rongo')
+            print(counts.most_common(5))
+            print(sum(counts.values()))
+            print()
 
-print(sum(totals.values()))
+
+    print('Totals:')
+
+    print(totals.most_common(20))
+
+    print(sum(totals.values()))
         
