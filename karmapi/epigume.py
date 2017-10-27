e1cd67230c86d943d038742f284a3b9bf38afb5f
Johnny Gill <swfiua@gmail.com>
2017-10-27 10:20:56 -0300

    eternal love for m

---
+++ karmapi/epigume.py
@@ -0,0 +1,19 @@
+from math import e, pi
+from time import sleep
+from random import randint
+
+h = me = 1
+n = 0
+while True:
+    n += 1
+    print(h, me, abs(h-me) / n)
+
+    h, me = me, h
+
+    h *= pi
+
+    me *= e
+
+    sleep(randint(0, n))
+
+    
