4245a7c8aaa7d1a616ae19db94f869a02859c4f4
Johnny Gill <swfiua@gmail.com>
2018-05-13 16:24:42 -0400

    create a random ball

--- karmapi/cpr.py
+++ karmapi/cpr.py
@@ -3,10 +3,30 @@ Not, CPR, but Colin P Rourke.
 
 Simulate nested spherical waves.
 """
+import argparse
+
+from karmapi import base, tpot
+
+from random import random, randint
 
 class Sphere:
 
-    def __init__(self):
+    def __init__(self, size=None):
+
+        size = size or 4
+
+        grid = []
+        for lat in range(size):
+            grid.append([random() for x in range(size)])
+
+        print(grid)
+
+if __name__ == '__main__':
+
+    ball = Sphere()
+            
+
+
 
 
         
