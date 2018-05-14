e30876834d66ed0b0a2e7693e469a6bd60333e73
Johnny Gill <swfiua@gmail.com>
2018-05-13 20:21:29 -0400

    borrowing from beanstalk for nested balls

--- karmapi/cpr.py
+++ karmapi/cpr.py
@@ -5,6 +5,8 @@ Simulate nested spherical waves.
 """
 import argparse
 
+import curio
+
 from karmapi import base, tpot
 
 from random import random, randint
@@ -21,10 +23,51 @@ class Sphere:
 
         print(grid)
 
+    async def run(self):
+        pass
+
+class NestedWaves(pigfarm.Yard):
+    pass
+
+    async def run(self):
+        pass
+
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--gallery', nargs='*', default=['.', '../gallery'])
+    parser.add_argument(
+        '--snowy', action='store_true',
+        help='random cat pictures')
+    parser.add_argument(
+        '--name', default='tree',
+        help='what to show')
+                            
+
+    args = parser.parse_args()
+
+    farm = pigfarm.PigFarm()
+    
+    from karmapi.mclock2 import GuidoClock
+    
+    farm.add(GuidoClock)
+
+    name = args.name
+    if args.snowy:
+        name = 'cat'
+        
+    farm.add(NestedWaves, dict(gallery=args.gallery, name=name))
+
+    curio.run(farm.run(), with_monitor=True)
+    
+
+        
+            
+
 if __name__ == '__main__':
 
-    ball = Sphere()
-            
+    main()
 
 
 
