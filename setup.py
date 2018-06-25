a755a9c498f197db5f11ff34cdb050c075c33f5a
Johnny Gill <swfiua@gmail.com>
2018-06-25 14:40:27 -0400

    world cup release 0.9.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '0.9.2.0'
+__version__ = '0.9.3.0'
 
 here = path.abspath(path.dirname(__file__))
 
