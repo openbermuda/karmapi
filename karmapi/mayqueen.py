444ab75ebb6ef13b3dfe344dcb9f80e5495ffd30
Johnny Gill <swfiua@gmail.com>
2018-07-19 20:08:30 -0400

    shine zodiacal light on global data with tea pots

---
+++ karmapi/mayqueen.py
@@ -0,0 +1,20 @@
+"""
+Shine some zodiacal light on spherical data
+
+
+"""
+
+from karmapi import cpr, tpot
+
+class MayQueen(cpr.Sphere):
+
+
+    async def run(self):
+        """ Run surrounding spheres through tea pots 
+
+        """
+        pass
+
+
+    
+    
