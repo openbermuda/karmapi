96d3f89d8305fdda55c0eaca5739c0a8def8f4a5
Johnny Gill <swfiua@gmail.com>
2018-08-22 13:30:16 -0400

    round and round with versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '1.031'
+__version__ = '1.0314'
 
 here = path.abspath(path.dirname(__file__))
 
