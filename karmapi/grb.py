7715a9aef6dd2cece8f968cbe8f9eedb6f16afbf
Johnny Gill <swfiua@gmail.com>
2018-10-17 16:25:54 -0400

    grasping at rays in the dark

--- karmapi/grb.py
+++ karmapi/grb.py
@@ -1,17 +1,65 @@
+"""
+Are gamma-ray bursts optical illusions?
+
+Robert S MacKay, Colin Rourke
+
+http://msp.warwick.ac.uk/~cpr/paradigm/GammaRayBursts.pdf
+
+What is it like when a galaxy emerges into view at the edge of our visible
+universe?
+
+In what follows I am imagining a wider universe that has the similar uniform
+structure.
+
+It appears spirals are a natural thing to arise and perhaps we just see a
+window on an expanding arm of a giant spiral of galaxies.
+
+If we go with this, then from the perspective of the galaxy (the emitter of
+light just coming into view from our world) nothing unusual is happening, it is
+just cruising along, just like our galaxy.
+
+What paths have the light waves have taken to get to us and how
+long that journey was?
+
+Note also the new addition to our inertial field, settling in.
+
+"""
+
 import math
+import numpy as np
 
 from matplotlib import pyplot as pp
+
+
 
 T = 1000
 k = 10000
 
 xx = [x * math.pi / k for x in range(T)]
-shint = [math.sinh(x) for x in xx]
-cosht = [math.cosh(x) for x in xx]
-
+shint = np.array([math.sinh(x) for x in xx])
+cosht = np.array([math.cosh(x) for x in xx])
+print(shint.size)
 #print(xx[-100:])
 #print(yy[-100:])
 
-pp.plot(shint, cosht)
+pp.plot(list(shint), list(cosht))
+
+
+# emitter
+shinu = shint.copy()
+coshu = cosht.copy()
+
+alpha = 2
+beta  = 1
+gamma = -1
+delta = 1
+e0 = (alpha * shinu) + (beta * coshu)
+e1 = (gamma * shinu) + (delta * coshu)
+
+# 2.2
+
+geo_test = - (e0 * shint) + (e1 * cosht)
+
+pp.plot(geo_test)
 
 pp.show()
