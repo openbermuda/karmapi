ed8e3efdd9218a743bc11d27ae8cb137aa9dd373
Johnny Gill <swfiua@gmail.com>
2019-05-13 19:29:56 -0400

    thinking about pipig backend

---
+++ karmapi/backends/pipig.py
@@ -0,0 +1,40 @@
+"""
+A default backend.
+
+Or a new beginning?
+
+After many adventures, let's go with simple Tk, with a twist.
+
+Just display the latest image from the deque.
+
+pigfarm: Space Yard PillBox and MagicCarpet
+
+   Space the base
+
+   Yard with at Canvas as an artist
+
+   PillBox: goal is a PIL.Image as artist
+
+   MagicCarpet:
+       matplotlib and pandas world???
+
+All come with different artists.
+
+pipig will focus on PIL, images, grids of data, often spherical.
+
+With the Canvas conversion to an image can be problematic.  
+
+The PIL.Draw api is rich enough for karma pi needs and on a par with the canvas
+api, so I don't need both here, let's stick to images.
+
+matplotlib will return images that can be similarly displayed, so I think we
+may be good.
+
+Whereas adding an image to tk for display purposes is (relatively) seamless (?)
+
+So on the python side then PIL, or rather pillow
+
+Grids or projections from spheres
+
+
+"""
