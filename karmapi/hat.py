39b4f1df113f07f95fda8785578a81a445aa9832
Johnny Gill <swfiua@gmail.com>
2021-09-29 16:42:16 -0400

    hat hackfest continues

--- karmapi/hat.py
+++ karmapi/hat.py
@@ -26,30 +26,32 @@ class HatInfo:
         h = self.hat
         while True:
 
-            if not self.stats:
+            if self.stats:
+            
+                messages = [
+                    f'T: {h.temp:0.1f}',
+                    f'P: {h.pressure:0.1f}',
+                    f'H: {h.humidity:0.1f}',
+                    f'TT: {self.whattimeisit()}']        
+
+                colours = [
+                    [255, 255, 0],
+                    [255, 0, 255],
+                    [0, 255, 255],
+                    [255, 255, 255],
+                ]
+                     
+
+                for message, colour in zip(messages, colours):
+                    
+                    h.show_message(message, text_colour=colour,
+                                   scroll_speed=0.2)
+
+            else:
                 self.hat.set_pixels([self.colour] * 64)
                 
                 time.sleep(2)
-            
-            messages = [
-                f'T: {h.temp:0.1f}',
-                f'P: {h.pressure:0.1f}',
-                f'H: {h.humidity:0.1f}',
-                f'TT: {self.whattimeisit()}']        
-
-            colours = [
-                [255, 255, 0],
-                [255, 0, 255],
-                [0, 255, 255],
-                [255, 255, 255],
-            ]
-                 
-
-            for message, colour in zip(messages, colours):
-                
-                h.show_message(message, text_colour=colour,
-                               scroll_speed=0.2)
-
+                continue
 
 if __name__ == '__main__':
 
